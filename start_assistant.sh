6549111549013e2a147d641c085a9305d8e82bc0
World-Teck-Studio <47188391+World-Teck-Studio@users.noreply.github.com>
2020-09-05 18:42:39 +0200

    Update start_assistant.sh

--- start_assistant.sh
+++ start_assistant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /home/pi/env/bin/activate
-googlesamples-assistant-pushtotalk --device-model-id raspy-d5ca4-raspigoogle-ha1k24
+googlesamples-assistant-pushtotalk --device-model-id ICCCCI
 
